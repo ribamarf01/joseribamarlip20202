d25831c5d2f84bb96b150e89901ac27309655211
ribamarf01 <ribamar.f01@gmail.com>
2020-12-02 11:03:02 -0300

    Exercicio 1 concluido

---
+++ Exercicios/Exercicio 01/maiormenor.f95
@@ -0,0 +1,30 @@
+program maiormenor
+    implicit none
+    integer, dimension(99) :: Int_List
+    integer :: List_Len, Maior, Menor, Counter
+
+    List_Len = 10
+
+    if((List_Len > 0) .and. (List_Len < 100)) then
+        
+        do Counter = 1, List_Len
+            read *, Int_List(Counter)
+        end do
+
+        Maior = Int_List(1)
+        Menor = Int_List(1)
+
+        do Counter = 2, List_Len
+            if(Maior > Int_List(Counter)) then
+                Maior = Int_List(Counter)
+            else if(Menor < Int_List(Counter)) then
+                Menor = Int_List(Counter)
+            end if
+        end do
+
+        print *, "Maior número da lista é ", Maior
+        print *, "Menor número da lista é ", Menor
+
+    end if
+
+end program maiormenor
---
+++ Exercicios/Exercicio 01/sort.f95
@@ -0,0 +1,33 @@
+program sort
+    implicit none
+
+    integer, dimension(99) :: Int_List
+    integer :: Swap, Counter, Counter2, List_Len
+
+    read *, List_Len
+
+    if((List_Len > 0) .and. (List_Len < 100)) then
+
+        do Counter = 1, List_Len
+            read *, Int_List(Counter)
+        end do
+
+        do Counter = 1, List_Len
+            do Counter2 = 2, List_Len
+                if(Int_List(Counter2 - 1) > Int_List(Counter2)) then
+                    Swap = Int_List(Counter2)
+                    Int_List(Counter2) = Int_List(Counter2 - 1)
+                    Int_List(Counter2 - 1) = Swap
+                end if 
+            end do
+        end do
+
+        print *, "Imprimindo números ordenados: "
+
+        do  Counter = 1, List_Len
+            print *, Int_List(Counter)
+        end do
+
+    end if
+
+end program sort
